c130265a44fddacb0461987b40238d6b0df1ac54
Nicole Bond <nicbond22@gmail.com>
2024-05-12 22:47:28 -0400

    Initial Commit

---
+++ CameraTutorial/CameraTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTutorialApp.swift
+//  CameraTutorial
+//
+//  Created by Scholar on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
